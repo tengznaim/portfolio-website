7031c7f4331224680ac1d83e92c8740ea0850657
Tengku Naim <naimtmas@gmail.com>
2021-02-11 23:12:09 +0800

    continued design on about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,7 +4,27 @@ import "./About.css";
 function About() {
   return (
     <div className="about-container" id="about">
-      <h1 className="test">About</h1>
+      <div className="content-container">
+        <h1 className="about-item" id="about-tag">
+          &lt;About&gt;
+        </h1>
+        <h1 className="about-item" id="about-greeting">
+          Hello!
+        </h1>
+        <p className="about-item" id="about-paragraph">
+          I'm <span id="about-highlighted">Naim</span>! I'm a{" "}
+          <span id="about-highlighted">Computer Science </span>
+          student specialising in{" "}
+          <span id="about-highlighted">Artificial Intelligence</span> (expected
+          graduation: 2023). Despite my specialisation, I enjoy exploring the
+          various fields of CS and am always open to learning new things. I also
+          pride myself with having a creative side and really love cookies (the
+          edible kind). 🍪
+        </p>
+        <button className="about-item" id="contact-button">
+          Get in Touch
+        </button>
+      </div>
     </div>
   );
 }
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,21 +4,27 @@ import { Link } from "react-router-dom";
 import "./Logo.css";
 
 function Logo() {
-  const [logoVisible, setVisibility] = useState(true);
+  const [logoVisible, setLogoVisible] = useState(true);
   const [scrollPos, setScrollPos] = useState(0);
+
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
-  }, []);
 
-  function handleScroll() {
-    const curr = document.body.getBoundingClientRect().top;
-    if (curr <= -80 && curr < scrollPos) {
-      setVisibility(false);
-    } else {
-      setVisibility(true);
+    function handleScroll() {
+      const curr = document.body.getBoundingClientRect().top;
+      if (curr <= -80 && curr < scrollPos) {
+        setLogoVisible(false);
+      } else if (curr > scrollPos) {
+        setLogoVisible(true);
+      }
+      setScrollPos(curr);
     }
-    setScrollPos(curr);
-  }
+
+    //Cleanup Function
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [scrollPos]);
 
   return (
     <div className={logoVisible ? "logo-container" : "logo-container hidden"}>
